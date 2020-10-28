6d76b22846cb4f9a3eb4ed193c8a706869915754
AnonShad0w <13724916+AnonShad0w@users.noreply.github.com>
2020-10-28 15:01:47 -0700

    Create missingInt.py

---
+++ missingInt.py
@@ -0,0 +1,32 @@
+"""
+Write a function:
+
+def solution(A)
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+"""
+
+def solution(A):
+    arr = [0] * (len(A) + 1)
+    A.sort()
+    for a in A:
+        print(f'1st for:{a}')
+        if a > 0:
+            arr.insert(a, 1)
+    print(A)
+    print(arr[:len(A) + 1])
+    for i in range(1, (len(A) + 1)):
+        print(f'2nd for:{i}, arr[i]{arr[i]}')
+        if arr[i] == 0:
+            return i
